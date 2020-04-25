746fae29c5551c8f0821f2751e8bf5606f107fe6
ademarCardoso <ac@storyblok.com>
2020-04-25 20:17:15 -0300

    Add initial content

--- components/Container.vue
+++ components/Container.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="container content is-medium">
     <section>
-      <a href="#ideia">Idéia</a>
-      <p>A idéia do prjeto veio de marte e seus habitantes verdes</p>
+      <a href="#proposta">Proposta do Projeto</a>
+      <p>
+        A proposta da impressora Print Dots é ser uma impressora de Braille de
+        baixo custo, onde por meio de software e hardware a impressora seja 
+        eficiente e versátil e que qualquer pessoa consiga à utilizar, 
+        a impressora é simples e quase que sem configurações, para que 
+        até pessoas leigas a utilizem de maneira fácil.
+      </p>
+      <p>
+        Um dos pontos que faz essa impressora trativa é o fato de por ela ser barata, 
+        escolas menores com pouco caixa para gastos o que é uma triste realidade brasileira,
+        qualquer pessoa poderá comprá-la, pois seu baixo custo proporciona isso, e como 
+        ela é simples não se faz necessário nenhum conhecimento especifico.
+      </p>
+      <p>
+        A simplicidade da Print Dots não significa que ela não seja robusta e ou 
+        não seja versátil, pelo contrário, a idéia é que seja um impressora de baixo custo
+        porém que seja tão boa quanto as impressoras de braille existentes, mas 
+        claro, custando uma fração do valor.
+      </p>
+      <a href="#como-funciona">Como a impressora funciona ?</a>
+      <p>
+        A impressora funciona de maneira simples, você a conecta em um computador 
+      </p>
     </section>
   </div>
 </template>
